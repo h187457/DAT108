8c80f3c638ad1b65ad3cd41155532ecb57e13ecb
h597236 <597236@stud.hvl.no>
2025-09-16 13:10:43 +0200

    Ferdig hamburger class

--- oblig2Troder/oppgave2/hamburger.java
+++ oblig2Troder/oppgave2/hamburger.java
@@ -1,4 +1,13 @@
 package oppgave2;
 
 public class hamburger {
+    private final int id;
+    public hamburger(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "◖" + id + '◗';
+    }
 }
