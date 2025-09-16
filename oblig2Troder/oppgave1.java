fa3b4fb8c313567c919f597c65fd76fb7796f0bb
Abdul Al kadri <187457@stud.hvl.no>
2025-09-16 11:20:11 +0200

    oppgave1

---
+++ oblig2Troder/oppgave1.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class oppgave1 {
+    private static volatile boolean running = true;
+    private static volatile String message = "Hallo verden!";
+
+    public static void main(String[] args) {
+
+
+        
+        Thread printer = new Thread(() -> {
+            while (running) {
+                System.out.println(message);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ignored) {}
+            }
+        });
+        printer.start();
+
+        Thread input = new Thread(() -> {
+            while (running) {
+                String in = JOptionPane.showInputDialog("Skriv melding (eller 'quit'):");
+                if (in == null) continue;
+                if (in.equalsIgnoreCase("quit")) {
+                    running = false;
+                } else {
+                    message = in;
+                }
+            }
+        });
+        input.start();
+    }
+}
